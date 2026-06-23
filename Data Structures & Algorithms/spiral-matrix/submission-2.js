3b5f712e913fc260a07add33a28e1bd442ae6f02
Huanhuan <leochh5@gmail.com>
2026-06-23 09:47:25 +0800

    Add: spiral-matrix - submission-2

---
+++ Data Structures & Algorithms/spiral-matrix/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        const m = matrix.length;
+        const n = matrix[0].length;
+        let l = 0,
+            r = n - 1,
+            t = 0,
+            b = m - 1;
+        const res = [];
+        while (res.length < m * n) {
+            for (let j = l; j <= r; j++) {
+                res.push(matrix[t][j]);
+            }
+            t++;
+            for (let i = t; i <= b; i++) {
+                res.push(matrix[i][r]);
+            }
+            r--;
+            if (t <= b) {
+                for (let j = r; j >= l; j--) {
+                    res.push(matrix[b][j]);
+                }
+                b--;
+            }
+            if (l <= r) {
+                for (let i = b; i >= t; i--) {
+                    res.push(matrix[i][l]);
+                }
+                l++;
+            }
+        }
+        return res;
+    }
+}
