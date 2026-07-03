9e39104e606a45a0c8a312a974506f939c9dc9ae
Huanhuan <leochh5@gmail.com>
2026-07-03 19:18:31 +0800

    Add: swim-in-rising-water - submission-8

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-8.js
@@ -0,0 +1,28 @@
+class Solution {
+    swimInWater(grid) {
+        const m = grid.length;
+        const n = grid[0].length;
+        const dirs = [
+            [-1, 0],
+            [1, 0],
+            [0, -1],
+            [0, 1],
+        ];
+        const mq = new MinPriorityQueue((a) => a[0]); // [t, i, j]
+        mq.push([grid[0][0], 0, 0]);
+        const visited = Array.from({ length: m }, () => Array(n).fill(false));
+
+        while (!mq.isEmpty()) {
+            const [t, i, j] = mq.dequeue();
+            if (i === m - 1 && j === n - 1) return t;
+            for (const [dx, dy] of dirs) {
+                const x = i + dx;
+                const y = j + dy;
+                if (x < 0 || y < 0 || x >= m || y >= n) continue;
+                if (visited[x][y]) continue;
+                visited[x][y] = true;
+                mq.push([Math.max(t, grid[x][y]), x, y]);
+            }
+        }
+    }
+}
