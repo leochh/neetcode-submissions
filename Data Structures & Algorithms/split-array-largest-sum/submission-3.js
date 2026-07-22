5c025ee76111d24f07cd7cfda099c2c330e9eaa8
Huanhuan <leochh5@gmail.com>
2026-07-22 19:34:32 +0800

    Add: split-array-largest-sum - submission-3

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-3.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    splitArray(nums, k) {
+        const prefix = [0];
+        for(const n of nums) {
+            prefix.push(prefix.at(-1) + n);
+        }
+
+        const feasible = (x) => {
+            let groups = k;
+            let last = 0;
+            while(groups) {
+                let l = last + 1, r = prefix.length;
+                while(l < r) {
+                    const m = l + Math.floor((r - l) / 2);
+                    if(prefix[m] > prefix[last] + x) {
+                        r = m;
+                    } else {
+                        l = m + 1;
+                    }
+                }
+                last = l - 1;
+                groups--;
+            }
+            return last == prefix.length - 1;
+        }
+
+        let l = Math.max(...nums), r = nums.reduce((a, b) => a + b, 0);
+        while(l < r) {
+            const m = l + Math.floor((r - l) / 2);
+            if(feasible(m)) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
