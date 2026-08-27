c7b574c1560992719458d85cdf48864fdd144fe1
Huanhuan <leochh5@gmail.com>
2026-08-27 23:46:46 +0800

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,15 @@
+class Solution {
+    /* */
+    twoSum(nums, target) {
+       const map = new Map();
+       for(let i = 0; i < nums.length; i++) {
+            const n = nums[i];
+            const comp = target - n;
+            if(map.has(comp)) {
+                return [i, map.get(comp)];
+            }
+            map.set(n, i);
+       }
+       return [];
+    }
+}
