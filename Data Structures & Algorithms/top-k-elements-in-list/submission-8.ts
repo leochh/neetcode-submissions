d531ee6cdfe321f8f3972afebce723e6e69ec6d0
Huanhuan <leochh5@gmail.com>
2026-08-28 17:37:38 +0800

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const map = new Map();
+        for(const n of nums) {
+            map.set(n, (map.get(n) || 0) + 1);
+        }
+
+        const heap = new MinPriorityQueue(a => a[1]);
+        for(const entry of map) {
+            heap.enqueue(entry);
+            if(heap.size() > k) heap.dequeue();
+        }
+        return heap.toArray().map(v => v[0]);
+    }
+}
