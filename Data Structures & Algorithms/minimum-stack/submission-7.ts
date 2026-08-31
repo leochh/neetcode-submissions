e6125b22fc8b33572245960afaa8a182117ad61b
Huanhuan <leochh5@gmail.com>
2026-08-31 12:48:52 +0800

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.ts
@@ -0,0 +1,45 @@
+class MinStack {
+    min: number;
+    values: number[] = [];
+
+    constructor() {
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        if(!this.values.length) {
+            this.values.push(0);
+            this.min = val;
+        } else {
+            this.values.push(val - this.min);
+            if(val < this.min) this.min = val;
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        if (this.values.length === 0) return;
+        const val = this.values.pop();
+        if(val < 0) this.min -= val;
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        const top = this.values.at(-1);
+        return top > 0 ? top + this.min : this.min;
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.min;
+    }
+}
