d3aad5b86a26264b73445021d1d37bb63c1d5c5d
Huanhuan <leochh5@gmail.com>
2026-09-01 11:00:37 +0800

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n: number): number {
+        const dp = Array(n + 1).fill(0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(let i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp.at(-1);
+    }
+}
