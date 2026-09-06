f55e37a21140a1ea8c3b05e2b2851efd18f3139e
Huanhuan <leochh5@gmail.com>
2026-09-06 20:59:18 +0800

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.ts
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {number} a
+     * @param {number} b
+     * @return {number}
+     */
+    getSum(a: number, b: number): number {
+        return Number(BigInt(a) + BigInt(b));
+    }
+}
